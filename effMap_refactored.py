13ac232e52d3d1b014699ac112ff97907eef612f
Okhotnikov <iokhotnikov@hydreco.com>
2020-06-20 12:27:05 +0100

    Plotting details changed

--- effMap_refactored.py
+++ effMap_refactored.py
@@ -8,9 +8,10 @@ oils = {'15w40': {'visc_kin': 13.65, 'density': 829.1, 'visc_dyn': 13.65 * 829.1
         '10w40': {'visc_kin': 14.61, 'density': 804.5, 'visc_dyn': 14.61 * 804.5 / 1e6, 'bulk': 15000}}
 engines = {'engine_1': {'speed': [1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000, 2100, 2200, 2300, 2400, 2500, 2600, 2700, 2800, 2900, 3000],
                         'torque': [1350, 1450, 1550, 1650, 1800, 1975, 2200, 2450, 2750, 3100, 3100, 3100, 3100, 3022, 2944, 2849, 2757, 2654, 2200, 1800, 0],
-                        'power': [141.372, 167.028, 194.779, 224.624, 263.894, 310.232, 368.614, 436.158, 518.363, 616.799, 649.262, 681.726, 714.189, 727.865, 739.908, 745.866, 750.652, 750.401, 645.074, 546.637, 0.000]},
+                        'power': [141.372, 167.028, 194.779, 224.624, 263.894, 310.232, 368.614, 436.158, 518.363, 616.799, 649.262, 681.726, 714.189, 727.865, 739.908, 745.866, 750.652, 750.401, 645.074, 546.637, 0]},
            'engine_2': {'speed': [600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000, 2100, 2200, 2300, 2400],
-                        'torque': [1000, 1100, 1450, 1750, 2100, 2400,	2600, 2950, 3100, 3300, 3400, 3500, 3400, 3300, 3200, 3000,	2800, 2600, 0]}}
+                        'torque': [1000, 1100, 1450, 1750, 2100, 2400, 2600, 2950, 3100, 3300, 3400, 3500, 3400, 3300, 3200, 3000, 2800, 2600, 0],
+                        'power': [62.8319, 80.634, 121.475, 164.934, 219.911, 276.46, 326.726, 401.6, 454.484, 518.363, 569.675, 623.083, 640.885, 656.593, 670.206, 659.734, 645.074, 626.224, 0]}}
 
 
 class HSU:
@@ -20,16 +21,17 @@ class HSU:
     ----------
     disp : int
         The displacement of an axial-piston machine in cc/rev
-    sa : int, optional
-        The maxivisc_dynm swash angle of the axial piston machine in degrees, default 18 degrees when optional.
-    z : int, optional
+    swash : int, optional
+        The maxi swash angle of the axial piston machine in degrees, default 18 degrees when optional.
+    pistons : int, optional
         The number of piston in a machine, default 9 when optional.
     oil : {'15w40', '5w30', '10w40'}, optional
-        Choices of available oils, default '15w40'. Each is a dictionary with the following structure: {'visc_kin': float, 'density': float, 'bulk': float}.
-        Here 'visc_kin' is the kinematic viscosity of the oil in cSt, 'density' is its density in kg/cub.m, 'bulk' is the bulk modulus in bar. All properties are at 100C.
+        The oil choice from the dictionary of available oils , default '15w40'. Each oil is a dictionary with the following structure: {'visc_kin': float, 'density': float, 'visc_dyn': float, 'bulk': float}. Here 'visc_kin' is the kinematic viscosity of the oil in cSt, 'density' is its density in kg/cub.m, 'visc_dyn' is the dynamic viscosity in Pa s, 'bulk' is the oil bulk modulus in bar. All properties are at 100C.
+    engine : {'engine_1', 'engine_2'}, optional
+        The engine choice from the dictionary of engines, default 'engine_1'. Each engine is a dictionary with the following structure: {'speed': list, 'torque': list, 'power': list}. Lists must be of the same length.
     """
 
-    def __init__(self, disp, swash=18, pistons=9, oil='15w40', engine='engine_1', input_gear_ratio=.75):
+    def __init__(self, disp, swash=18, pistons=9, oil='15w40', engine='engine_1', input_gear_ratio=.75, max_power_input=682):
         self.displ = disp
         self.swash = swash
         self.pistons = pistons
@@ -38,8 +40,9 @@ class HSU:
         self.efficiencies = {}
         self.performance = {}
         self.leaks = {}
-        self.input_gear_ratio = input_gear_ratio
         self.engine = engine
+        self.input_gear_ratio = input_gear_ratio
+        self.max_power_input = max_power_input
         self.size()
 
     def size(self, k1=.75, k2=.91, k3=.48, k4=.93, k5=.91):
@@ -137,7 +140,7 @@ class HSU:
                       'HSU': {'Block': 2*leak_block, 'Shoes': 2*leak_shoes, 'Pistons': 2*leak_pistons, 'Total': 2*leak_total}}
         return self.efficiencies
 
-    def plot_eff_map(self, max_speed_pump, max_pressure_discharge, min_speed_pump=1000, min_pressure_discharge=100, pressure_charge=25.0, res=100, show_figure=True, save_figure=False):
+    def plot_eff_map(self, max_speed_pump, max_pressure_discharge, min_speed_pump=1000, min_pressure_discharge=75, pressure_charge=25.0, res=100, show_figure=True, save_figure=False):
         """Plots and saves the HSU efficiency map.
 
         Parameters
@@ -171,20 +174,19 @@ class HSU:
              for j in pressure]
         )
         torque_pump = self.displ * 1e-6 * (pressure - pressure_charge) * 1e5 / \
-            (2 * np.pi * np.amax(mech_eff_pump, axis=1) * 1e-2)
-        fig = make_subplots(specs=[[{"secondary_y": True}]])
+            (2 * np.pi * np.amax(mech_eff_pump, axis=0) * 1e-2)
+        fig = go.Figure()
         fig.add_trace(
             go.Contour(
                 z=eff_hsu,
                 x=speed,
-                y=pressure,
+                y=torque_pump,
                 colorscale='Portland',
                 showscale=False,
                 contours_coloring='lines',
                 name='Total HSU efficiency, %',
                 contours=dict(
                     coloring='lines', start=50, end=90, size=1, showlabels=True, labelfont=dict(size=8, color='black',))),
-            secondary_y=False,
         )
         fig.update_layout(
             title="HSU efficiency map and the engine torque curve",
@@ -193,18 +195,26 @@ class HSU:
                 showline=True,
                 linecolor='black',
                 mirror=True,
+                showgrid=True,
+                gridcolor='LightGray',
+                gridwidth=0.25,
+                linewidth=0.5,
             ),
             yaxis=dict(
-                title="HSU discharge pressure, bar",
+                # title="HSU discharge pressure, bar",
                 showline=True,
                 linecolor='black',
                 mirror=True,
+                showgrid=True,
+                gridcolor='LightGray',
+                gridwidth=0.25,
+                linewidth=0.5,
             ),
             plot_bgcolor='#fff',
             paper_bgcolor='#fff',
             showlegend=True,
         )
-        if self.engine and self.input_gear_ratio:
+        if self.engine:
             fig.add_trace(
                 go.Scatter(
                     x=self.input_gear_ratio * np.asarray(
@@ -212,20 +222,27 @@ class HSU:
                     y=np.asarray(engines[self.engine]
                                  ['torque']) / self.input_gear_ratio,
                     name="Available engine torque",
+                    line=dict(color='indianred', width=1),
                 ),
-                secondary_y=True,
+                # secondary_y=True,
             )
-            fig.update_yaxes(
-                title_text='HSU input torque, Nm',
-                secondary_y=True,
-                # showline=True,
-                # linecolor='black',
+            fig.add_trace(
+                go.Scatter(
+                    x=speed,
+                    y=self.max_power_input * 1e3 * 30 /
+                    (np.pi * speed),
+                    name=f"Torque at {self.max_power_input} kW",
+                    line=dict(color='steelblue', width=1),
+                ),
             )
             fig.update_layout(
                 showlegend=True,
                 xaxis=dict(
                     dtick=200,
                     range=[min_speed_pump, max_speed_pump],
+                ),
+                yaxis=dict(
+                    range=[np.amin(torque_pump), np.amax(torque_pump)],
                 ),
             )
         if save_figure:
@@ -239,8 +256,7 @@ class HSU:
 if __name__ == '__main__':
     hsu = HSU(440,
               #   engine=None,
-              #   input_gear_ratio=None,
               )
-    hsu.plot_eff_map(2400, 600,
-                     #   save_figure=True,
+    hsu.plot_eff_map(2300, 650,
+                     #  save_figure=True,
                      )
