1a6afb9eeece2c162f2b0de1eddee71f2501c40d
Okhotnikov <iokhotnikov@hydreco.com>
2020-10-07 12:49:24 +0100

    Setup  url updated

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Custom regressor and HST object",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/ivanokhotnikov/effmap/tree/Test-Pypi-version",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
