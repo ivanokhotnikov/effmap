Motor forces calculation corrected

effmap/hst.py
@@ -299,7 +299,7 @@ class HST:
         return self.efficiencies
 
     def compute_loads(self, pressure_discharge, pressure_charge=25.0):
-        """Calculates steady state, pressure-induced structural loads in the HST.
+        """Calculates steady state, pressure-induced structural loads in the HST Forces in kN, torques in Nm.
 
         Parameters
         ----------
@@ -317,9 +317,9 @@ class HST:
         self.swash_hp_z = self.swash_hp_x * np.tan(np.radians(self.swash))
         self.swash_lp_z = self.swash_lp_x * np.tan(np.radians(self.swash))
         self.motor_hp = (self.pistons // 2 + 1) * pressure_discharge * \
-            1e5 * self.sizes['Ap'] / np.cos(np.radians(self.swash))
+            1e5 * self.sizes['Ap'] / np.cos(np.radians(self.swash)) / 1e3
         self.motor_lp = (self.pistons // 2) * pressure_charge * \
-            1e5 * self.sizes['Ap'] / np.cos(np.radians(self.swash))
+            1e5 * self.sizes['Ap'] / np.cos(np.radians(self.swash)) / 1e3
         self.shaft_torque = self.performance['pump']['torque']
 
     def add_no_load(self, *args):